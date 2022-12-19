b61947429deabaae9788d88fbd0a2c25d9f863c1
Marcin <marcin.p.dev@gmail.com>
2022-12-19 17:57:48 +0100

    Cleaned a bit code

--- script.js
+++ script.js
@@ -7,13 +7,5 @@ title.classList.add('transition');
 let titleText = document.getElementById('hero-title-desc');
 titleText.classList.add('transitionTwo'); 
 
-  //  let animationElements = document.getElementsByClassName('tran');
-  //  for (let i=0, i<animationElements.length,i++ )
-   
-  //  animationElements.forEach(function(element){
-  //    element.classList.add('transition');
-  //    console.log(animationElements[0]);
-  //    console.log(animationElements[1]);
-  //  });
 }
 
